88bd04e6f744b7f40044d0db0d1997058a832342
Ray O'Neill <ray.oneill@gmail.com>
2025-06-29 22:24:28 -0400

    additional tests for async action support

---
+++ src/BehaviourTree.Tests/ActionBehaviourTests.cs
@@ -0,0 +1,26 @@
+﻿using BehaviourTree.Behaviours;
+using BehaviourTree.Tests.Utils;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace BehaviourTree.Tests
+{
+    [TestFixture]
+    internal sealed class ActionBehaviourTests
+    {
+        [Test]
+        public void ActionBehavior_should_execute_async_action_synchronously()
+        {             
+            var executed = false;
+            var sut = new ActionBehaviour<MockContext>("TestAction", async ctx =>
+            {
+                await Task.Delay(100);
+                executed = true;
+                return BehaviourStatus.Succeeded;
+            });
+            var status = sut.Tick(new MockContext());
+            Assert.AreEqual(BehaviourStatus.Succeeded, status);
+            Assert.IsTrue(executed);
+        }
+    }
+}
--- src/BehaviourTree.Tests/ConditionTests.cs
+++ src/BehaviourTree.Tests/ConditionTests.cs
@@ -1,6 +1,7 @@
 ﻿using BehaviourTree.Behaviours;
 using BehaviourTree.Tests.Utils;
 using NUnit.Framework;
+using System.Threading.Tasks;
 
 namespace BehaviourTree.Tests
 {
@@ -18,9 +19,29 @@ namespace BehaviourTree.Tests
         }
 
         [Test]
+        public void WhenPredicateReturnTrue_ReturnSuccessAsync()
+        {
+            var sut = new Condition<MockContext>(async ctx => await Task.FromResult(true));
+
+            var behaviourStatus = sut.Tick(new MockContext());
+
+            Assert.That(behaviourStatus, Is.EqualTo(BehaviourStatus.Succeeded));
+        }
+
+        [Test]
         public void WhenPredicateReturnFalse_ReturnFailure()
         {
             var sut = new Condition<MockContext>(_ => false);
+
+            var behaviourStatus = sut.Tick(new MockContext());
+
+            Assert.That(behaviourStatus, Is.EqualTo(BehaviourStatus.Failed));
+        }
+
+        [Test]
+        public void WhenPredicateReturnFalse_ReturnFailureAsync()
+        {
+            var sut = new Condition<MockContext>(async ctx => await Task.FromResult(false));
 
             var behaviourStatus = sut.Tick(new MockContext());
 
